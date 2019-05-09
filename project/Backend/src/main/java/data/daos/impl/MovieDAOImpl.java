9e55ee5a090fac5ac63627e70d2c4183006b6950
Helena <a78633@alunos.uminho.pt>
2019-05-09 23:37:41 +0100

    :briefcase: Movie backend updated

--- project/Backend/src/main/java/data/daos/impl/MovieDAOImpl.java
+++ project/Backend/src/main/java/data/daos/impl/MovieDAOImpl.java
@@ -2,11 +2,25 @@ package data.daos.impl;
 
 import data.daos.MovieDAO;
 import data.entities.Movie;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Query;
 
 
 @Component("movieDAO")
 public class MovieDAOImpl extends DAOImpl<Integer , Movie> implements MovieDAO {
 
+    @Transactional(readOnly=true)
+    public Movie loadEntityEager(String condition) {
+        Query query =  entityManager.createQuery("SELECT c FROM "+ entityClass.getName() + " c WHERE " + condition);
+        Movie result = (Movie) query.getSingleResult();
+        // TODO: mudar para fetch
+        Hibernate.initialize(result.getGenres());
+        Hibernate.initialize(result.getCompanies());
+        return result;
+
+    }
 
 }
